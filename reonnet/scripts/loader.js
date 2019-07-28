1ce14f0fdd88651a3f2d1d6fc02542c825c1423b
Aswad Ali <aswadgemini11@gmail.com>
2019-07-28 18:06:28 +0500

    Added Proper JS Preloader

--- reonnet/scripts/loader.js
+++ reonnet/scripts/loader.js
@@ -1,6 +1,17 @@
-var loader = document.getElementById('loader');
-loader.addEventListener('animationend', function(e) {
-  if (e.animationName == 'scale-out') {
-    this.style.display = 'none';
-  }
+window.addEventListener('load', function() {
+  var loader = document.getElementById('loader');
+  var loadingGif = document.getElementById('loading-gif');
+
+  loader.classList.add('loaded');
+  loadingGif.classList.add('loaded');
+
+  loader.addEventListener('animationend', function(e) {
+    if (e.animationName == 'scale-out') {
+      loader.style.visibility = 'hidden';
+      document.querySelector('.header').classList.add('loaded');
+      document.querySelector('#header-text').classList.add('loaded');
+      document.querySelector('#menu-wrapper').style.opacity = 1;
+      document.querySelector('#menu-wrapper').style.visibility = 'visible';
+    }
+  });
 });
