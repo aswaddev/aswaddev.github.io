13dabe4121a43f82698f1f74358e64c1fad14176
Aswad Ali <aswadgemini11@gmail.com>
2020-05-03 22:37:30 +0500

    Blog feature complete with static content

--- script/script.js
+++ script/script.js
@@ -5,18 +5,46 @@ var overlayMenu = document.getElementById('overlay-menu');
 var navLinks = document.querySelectorAll('#overlay-menu ul li a');
 
 for (var link of navLinks) {
-	link.addEventListener('click', openNavbar);
+  link.addEventListener('click', openNavbar);
 }
 navButton.addEventListener('click', openNavbar);
 
 function openNavbar() {
-	if (navBurger.classList.contains('fa-times')) {
-		overlayMenu.style.left = "-100%";
-		navBurger.classList.remove('fa-times');
-		navBurger.classList.add('fa-bars');
-	} else {
-		overlayMenu.style.left = "0%";
-		navBurger.classList.remove('fa-bars');
-		navBurger.classList.add('fa-times');
-	}
+  if (navBurger.classList.contains('fa-times')) {
+    overlayMenu.style.left = '-100%';
+    navBurger.classList.remove('fa-times');
+    navBurger.classList.add('fa-bars');
+  } else {
+    overlayMenu.style.left = '0%';
+    navBurger.classList.remove('fa-bars');
+    navBurger.classList.add('fa-times');
+  }
 }
+
+(function () {
+  'use strict';
+
+  var section = document.querySelectorAll('section');
+  var sections = {};
+  var i = 0;
+
+  Array.prototype.forEach.call(section, function (e) {
+    sections[e.id] = e.offsetTop;
+  });
+
+  window.onscroll = function () {
+    var scrollPosition =
+      document.documentElement.scrollTop || document.body.scrollTop;
+
+    for (i in sections) {
+      if (sections[i] <= scrollPosition) {
+        if (document.getElementById(i + '-link')) {
+          if (document.querySelector('.active')) {
+            document.querySelector('.active').setAttribute('class', ' ');
+          }
+          document.getElementById(i + '-link').setAttribute('class', 'active');
+        }
+      }
+    }
+  };
+})();
